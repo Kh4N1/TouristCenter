f733d51632ede127a4d447425987e0b675472efd
Kh4N1 <arkhani104@outlook.com>
2023-08-20 17:06:28 +0330

    "Creating a Simple Tour Model" & "Creating Documents and Testing the Model"

--- server.js
+++ server.js
@@ -3,21 +3,65 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app.js");
 
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(
+        new Error(
+          `❌ Request took too long for getting DBs! Timeout after ${sec} second ❌`,
+        ),
+      );
+    }, sec * 1000);
+  });
+};
+
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD,
 );
 
-mongoose
-  // .connect(process.env.DATABASE_LOCAL, {
-  .connect(DB, {
+Promise.race([
+  mongoose.connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
+  }),
+  timeout(5),
+])
   .then(() => {
-    // console.log(con.connections);
     console.log("DB connection successful! ✅");
+  })
+  .catch((error) => {
+    console.error(`💥${error.message}`);
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "fadaei",
+  price: 899
+});
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.error(`ERROR ‼: ${err}`));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
