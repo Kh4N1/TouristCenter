007a0706ce489cc35ea64854c5f95cbaaf14e470
Kh4N1 <arkhani104@outlook.com>
2023-08-28 00:14:29 +0330

    a little update with bug

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -15,21 +15,29 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
+
+    // Programming or other unknown error: don't leak error details
   } else {
-    console.error("ERROR❌", err);
+    // 1) Log error
+    console.error("ERROR 💥", err);
+
+    // 2) Send generic message
     res.status(500).json({
-      status: err,
-      message: "SomeThings went very wrong!",
+      status: "error",
+      message: "Something went very wrong!",
     });
   }
 };
 
 module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
