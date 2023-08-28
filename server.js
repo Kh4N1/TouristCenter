4955d930e2ad8e89055b9c8183df2b00a2b97a06
Kh4N1 <arkhani104@outlook.com>
2023-08-28 15:49:33 +0330

    Fixed a bug in Unhandled Rejections

--- server.js
+++ server.js
@@ -3,32 +3,19 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app.js");
 
-const timeout = function (sec) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(
-        new Error(
-          `❌ Request took too long for getting DBs! Timeout after ${sec} second ❌`,
-        ),
-      );
-    }, sec * 1000);
-  });
-};
-
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD,
 );
 
-Promise.race([
-  mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }),
-  timeout(7),
-]).then(() => {
-  console.log("✅ DB connection successful! ");
-})
+  })
+  .then(() => {
+    console.log("✅ DB connection successful! ");
+  });
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
