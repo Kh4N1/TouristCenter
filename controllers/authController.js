c9624ec7107ee19b4e4bac61588b4571d094db1e
Kh4N1 <arkhani104@outlook.com>
2023-09-06 00:31:04 +0330

    💌 Sending JWT via Cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,16 @@ const signToken = (id) => {
 
 const creatSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+  res.cookie("jwt", token, cookieOptions);
+  //remove the password from the output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: "success",
@@ -37,7 +47,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordResetExpires: req.body.passwordResetExpires,
   });
 
-  creatSendToken(newUser, 201,res);
+  creatSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -189,5 +199,4 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 
   // 4- log user in, send JWT
   creatSendToken(user, 200, res);
-
 });
