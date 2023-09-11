210b138f6cb767b4cdc0cc4904ba9752a5851b2e
Kh4N1 <arkhani104@outlook.com>
2023-09-11 19:17:50 +0330

    an update

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -43,7 +43,6 @@ reviewSchema.pre(/^find/, function (next) {
 });
 
 reviewSchema.statics.calcAverageRatings = async function (tourId) {
-  console.log(tourId);
   // this keyword points to model
   const stats = await this.aggregate([
     {
@@ -57,7 +56,6 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
       },
     },
   ]);
-  console.log(stats);
   await Tour.findByIdAndUpdate(tourId , {
     ratingsQuantity: stats[0].nRating,
     ratingsAverage: stats[0].avgRating
