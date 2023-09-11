a52383c4a3011428702711adfbf4dc49f25a0745
Kh4N1 <arkhani104@outlook.com>
2023-09-11 20:07:21 +0330

    an update

--- models/tourModel.js
+++ models/tourModel.js
@@ -35,7 +35,7 @@ const tourSchema = new mongoose.Schema(
     },
     ratingsAverage: {
       type: Number,
-      default: 4.5,
+      default: 1,
       min: [1, "Rating must be above 1.0"],
       max: [5, "Rating must be below 5.0"],
     },
