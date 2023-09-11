dd9d9b5a82c7969d00bc9ac8967dbf534b0dfeeb
Kh4N1 <arkhani104@outlook.com>
2023-09-11 20:08:04 +0330

    fuuuckkkk  an update

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -78,7 +78,7 @@ reviewSchema.post("save", function () {
 // reviewSchema.post(/^findOneAnd/, async function() {
 //   // await this.findOne(); does NOT work here, query has already executed
 //   await this.r.constructor.calcAverageRatings(this.r.tour);
-});
+// });
 
 const Review = mongoose.model("Review", reviewSchema);
 
