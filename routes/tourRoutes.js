0f6a37b0389ad806779e90cab1cea0eec495aef1
Kh4N1 <arkhani104@outlook.com>
2023-09-13 10:06:37 +0330

    🥶👀 Geospatial Aggregation Calculating Distances

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -30,6 +30,8 @@ router
 // /tours-within?distance=233&center=-40,45&unit=mi
 // /tours-within/233/center/-40,45/unit/mi
 
+router.route("/distance/:latlng/unit/:unit").get(tourController.getDistance)
+
 router
   .route("/")
   .get(tourController.getAllTours)
