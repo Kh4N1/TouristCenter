0117fbbd1d7207c2a8c3528f79caf0042fe1c014
Kh4N1 <arkhani104@outlook.com>
2023-09-20 16:08:54 +0330

    Building the Tour Overview Part 1

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,11 +1,20 @@
-exports.getOverview = (req, res) => {
-    res.status(200).render("overview", {
-        title: "All tours",
-    });
-};
+const Tour = require("./../models/tourModel");
+const catchAsync = require("./../utils/catchAsync");
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1 - get tour data from collection
+  const tours = await Tour.find();
+  // 2 - build template
+
+  // 3 - render that template using tour data from 1
+  res.status(200).render("overview", {
+    title: "All tours",
+    tours
+  });
+});
 
 exports.getTour = (req, res) => {
-    res.status(200).render("tour", {
-        title: "The Forest Hiker Tour",
-    });
-}
+  res.status(200).render("tour", {
+    title: "The Forest Hiker Tour",
+  });
+};
