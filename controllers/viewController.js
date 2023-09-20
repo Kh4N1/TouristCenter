7e5e0d7e0ab756212c60736ca6bc005999e6ea2a
Kh4N1 <arkhani104@outlook.com>
2023-09-20 19:05:39 +0330

    Building the Tour Page part 1

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -5,16 +5,25 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   // 1 - get tour data from collection
   const tours = await Tour.find();
   // 2 - build template
-
+  console.log(tours)
   // 3 - render that template using tour data from 1
   res.status(200).render("overview", {
     title: "All tours",
-    tours
+    tours,
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // 1 - get the data, for the requested tour(including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: "reviews",
+    fields: "review rating user",
+  });
+  // 2 - build template
+
+  // 3 - render template using data form 1
   res.status(200).render("tour", {
     title: "The Forest Hiker Tour",
+    tour,
   });
-};
+});
