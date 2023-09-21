985c02d6638f2c5c8c7115a76cead63e0a7e4249
Kh4N1 <arkhani104@outlook.com>
2023-09-21 16:52:20 +0330

    Including a Map with Mapbox Part 2

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -14,10 +14,43 @@ console.log(locations);
 //   },
 // ).addTo(map);
 
+let averageLatitude = 0;
+let averageLongitude = 0;
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+locations.forEach(location => {
+    averageLatitude += location.coordinates[0];
+    averageLongitude += location.coordinates[1];
+});
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+averageLatitude /= locations.length;
+averageLongitude /= locations.length;
+
+var map = L.map('map').setView([averageLongitude, averageLatitude], 6);
+
+var Jawg_Light = L.tileLayer('https://{s}.tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+    attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: 4,
+    maxZoom: 22,
+    subdomains: 'abcd',
+    accessToken: 'IyWP5GvISCYl4AHX7MhGkEaDYdExDRx9O2JCc5QXulgdXdBxP4AR5JCsdHgP9oro'
+});
+
+Jawg_Light.addTo(map);
+// var marker = L.marker([39.7644863,-105.0199181]).addTo(map);
+var myIcon = L.icon({
+    iconUrl: '/img/pin.png',
+    iconSize: [38, 45],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+});
+locations.forEach(loc => {
+    // add marker
+    let lat = loc.coordinates[0]
+    let lng = loc.coordinates[1]
+    var marker = L.marker([lng,lat], {icon: myIcon}).addTo(map);
+    var popup = marker.bindPopup(`<h1>Day ${loc.day}: ${loc.description}</h1>`).openPopup();
+
+    // L.marker([loc.coordinates]).addTo(map)
+})
+
+
