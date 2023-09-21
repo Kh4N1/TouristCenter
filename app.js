ed1a71537586671e07835cdc59eca9235a4863d3
Kh4N1 <arkhani104@outlook.com>
2023-09-21 22:26:58 +0330

    Logging in Users with Our API Part 1

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xssClean = require("xss-clean");
 const hpp = require("hpp");
+const cookieParser = require("cookie-parser")
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -26,7 +27,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(helmet());
 app.use(helmet.xXssProtection());
 
-const csp = "script-src 'self' https://unpkg.com 'unsafe-inline'";
+const csp = "script-src 'self' https://unpkg.com https://cdnjs.cloudflare.com 'unsafe-inline'";
 app.use((req, res, next) => {
   res.header('Content-Security-Policy', csp);
   next();
@@ -47,6 +48,7 @@ app.use("/api", limiter);
 
 // body parser, reading data from body into req.body
 app.use(express.json({ limit: "100kb" }));
+app.use(cookieParser())
 
 // data Sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -71,6 +73,7 @@ app.use(
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies)
   // console.log((req.headers))
   next();
 });
