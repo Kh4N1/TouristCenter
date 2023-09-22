d95112728c1d10f66a6e8d81064f909853b69fcf
Kh4N1 <arkhani104@outlook.com>
2023-09-23 01:33:09 +0330

    Building the User Account Page AND Updating User Data

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xssClean = require("xss-clean");
 const hpp = require("hpp");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -27,9 +27,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(helmet());
 app.use(helmet.xXssProtection());
 
-const csp = "script-src 'self' https://unpkg.com https://cdnjs.cloudflare.com 'unsafe-inline'";
+const csp =
+  "script-src 'self' https://unpkg.com https://cdnjs.cloudflare.com 'unsafe-inline'";
 app.use((req, res, next) => {
-  res.header('Content-Security-Policy', csp);
+  res.header("Content-Security-Policy", csp);
   next();
 });
 
@@ -48,7 +49,8 @@ app.use("/api", limiter);
 
 // body parser, reading data from body into req.body
 app.use(express.json({ limit: "100kb" }));
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: true, limit: "100kb" }));
+app.use(cookieParser());
 
 // data Sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -80,7 +82,7 @@ app.use((req, res, next) => {
 
 // 2) routes
 
-app.use("/", viewRoutes)
+app.use("/", viewRoutes);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -35,7 +35,7 @@ const sendErrorDev = (err, req, res) => {
     });
   }
   // rendered website
-  console.error("ERROR 💥", err);
+  // console.error("ERROR 💥", err);
   return res.status(err.statusCode).render("error", {
     title: "Something went wrong!",
     msg: err.message,
@@ -54,7 +54,7 @@ const sendErrorProd = (err, req, res) => {
     }
     // Programming or other unknown error: don't leak error details
     // 1) Log error
-    console.error("ERROR 💥", err);
+    // console.error("ERROR 💥", err);
     // 2) Send generic message
     return res.status(500).json({
       status: "error",
@@ -71,7 +71,7 @@ const sendErrorProd = (err, req, res) => {
   }
   // Programming or other unknown error: don't leak error details
   // 1) Log error
-  console.error("ERROR 💥", err);
+  // console.error("ERROR 💥", err);
   // 2) Send generic message
   return res.status(err.statusCode).render("error", {
     title: "Something went wrong!",
